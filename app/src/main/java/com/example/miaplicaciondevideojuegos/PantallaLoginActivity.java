29b399ca50e7e156953ffe7ade9780891aa4a199
DESKTOP-IK6VUS6\pedro <pedrohm2704@gmail.com>
2024-05-24 04:04:13 +0200

    login

--- app/src/main/java/com/example/miaplicaciondevideojuegos/PantallaLoginActivity.java
+++ app/src/main/java/com/example/miaplicaciondevideojuegos/PantallaLoginActivity.java
@@ -2,8 +2,15 @@ package com.example.miaplicaciondevideojuegos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PantallaLoginActivity extends AppCompatActivity{
 
@@ -11,6 +18,38 @@ public class PantallaLoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallalogin);
+
+        Button botonIniciarSesion = findViewById(R.id.botonInicioSesion);
+        botonIniciarSesion.setOnClickListener(view -> {
+
+            EditText EditTextCorreo = findViewById(R.id.textoUsuarioInicioSesion);
+            EditText EditTextContra = findViewById(R.id.textoContraseña);
+
+
+            String correo = EditTextCorreo.getText().toString();
+            String contra = EditTextContra.getText().toString();
+
+            if (correo.isEmpty() || contra.isEmpty()) {
+
+                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+
+            } else {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(correo, contra)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                                Toast.makeText(this, "Inicio de sesion exitoso", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(this, PantallaPrincipalActivity.class);
+                                startActivity(intent);
+                            } else {
+
+                                Toast.makeText(this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
 
         TextView textoRegistrarse = findViewById(R.id.textoRegistrarse);
 
